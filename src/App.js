e37a08e3c9a1377fc19bc6686301b1dbbaf620fb
dianzy <snowflakednz@gmail.com>
2024-04-03 14:22:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return(
+  <div className="wrapper">
+    <div className="header">
+      <div className="csk">CHENNAI SUPER KINGS</div>
+    </div>
+    <div className="full">
+      <div className="inp">
+      <input type="text" placeholder="Text Box 1" className="text-box" />
+      <input type="text" placeholder="Text Box 2" className="text-box" />
+      </div>
+    </div>
+  </div>
+    );
+  }
+  
+  export default App;
+  
+
+  
+  
+  
+  
+  
+  
